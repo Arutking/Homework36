406718a55849354ba21ec661288405c7ea68f093
Arut Aerian <arut.aerian@icloud.com>
2023-03-09 14:48:45 +0100

    Homework36.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Soda добавка  = new Soda("малина");
+    System.out.println("Газировка и "+ добавка.GetMyDrinkString());
   }
 }
---
+++ src/Soda.java
@@ -0,0 +1,15 @@
+public class Soda {
+  //Attribute
+  private String добавка ;// malina
+
+
+  // constructor
+  public Soda(String малина ){
+    добавка = малина;
+  }
+
+  public String GetMyDrinkString(){
+    return добавка;
+  }
+}
+
